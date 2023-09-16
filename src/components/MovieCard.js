7dabb7917746774023b7968c3b8c0b7e597c5ce4
Naman Jawaliya <jawaliyan@gmail.com>
2023-09-16 22:20:59 +0530

    No hover on small and medium screen

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -4,14 +4,15 @@ import { IMAGE_CDN } from "../utils/constants/constants";
 const MovieCard = ({ alt, poster_path }) => {
   if (!poster_path) return null;
   return (
-    <div className="w-36 md:w-44 lg:w-56 cursor-pointer relative group hover:w-40 md:hover:w-48 lg:hover:w-60">
+    // hover:w-40 md:hover:w-48
+    <div className="w-36 md:w-44 lg:w-56 cursor-pointer relative group lg:hover:w-60">
       <img
         alt={alt}
         src={IMAGE_CDN + poster_path}
         className="rounded w-full h-full"
       />
-      <div className="hidden group-hover:block absolute inset-0 bg-black opacity-50 rounded"></div>
-      <div className="hidden group-hover:block text-white top-10 mx-8 absolute text-2xl font-bold z-10">
+      <div className="hidden lg:group-hover:block absolute inset-0 bg-black opacity-50 rounded"></div>
+      <div className="hidden lg:group-hover:block text-white top-10 mx-8 absolute text-2xl font-bold z-10">
         {alt}
       </div>
     </div>
