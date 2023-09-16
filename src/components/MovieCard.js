9e7fd826fa2e8dd10173e03255047dcdaa2a62fd
Naman Jawaliya <jawaliyan@gmail.com>
2023-09-17 00:49:32 +0530

    Update MovieCard.js

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -3,17 +3,19 @@ import { IMAGE_CDN } from "../utils/constants/constants";
 
 const MovieCard = ({ alt, poster_path }) => {
   if (!poster_path) return null;
+
   return (
-    // hover:w-40 md:hover:w-48
-    <div className="w-36 md:w-44 lg:w-56 cursor-pointer relative group lg:hover:w-60">
-      <img
-        alt={alt}
-        src={IMAGE_CDN + poster_path}
-        className="rounded w-full h-full"
-      />
-      <div className="hidden lg:group-hover:block absolute inset-0 bg-black opacity-50 rounded"></div>
-      <div className="hidden lg:group-hover:block text-white top-10 mx-8 absolute text-2xl font-bold z-10">
-        {alt}
+    <div className="w-36 md:w-44 lg:w-56 cursor-pointer relative group">
+      <div className="relative">
+        <img
+          alt={alt}
+          src={IMAGE_CDN + poster_path}
+          className="rounded w-full h-full transform transition-transform duration-300 group-hover:scale-110"
+        />
+        <div className="hidden group-hover:block group-hover:scale-110 absolute inset-0 bg-black opacity-50 rounded"></div>
+        <div className="hidden group-hover:block text-white top-10 mx-8 absolute text-lg font-bold z-10">
+          {alt}
+        </div>
       </div>
     </div>
   );
