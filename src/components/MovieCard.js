8b288359661b880c21190b5019df276338bbaafd
Naman Jawaliya <jawaliyan@gmail.com>
2023-09-17 12:53:43 +0530

    Update MovieCard.js

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -3,23 +3,30 @@ import {
   IMAGE_CDN,
   MOVIE_CARD_PLACEHOLDER,
 } from "../utils/constants/constants";
+import VoteIcon from "./Icons/VoteIcon";
 
-const MovieCard = ({ alt, poster_path }) => {
+const MovieCard = ({ alt, posterPath, voteAvg, voteCount }) => {
   return (
     <div className="w-36 md:w-44 lg:w-56 cursor-pointer relative group">
       <div className="relative transform transition-transform duration-300 group-hover:scale-110 rounded">
         <img
           alt={alt}
           src={
-            poster_path !== null
-              ? IMAGE_CDN + poster_path
+            posterPath !== null
+              ? IMAGE_CDN + posterPath
               : MOVIE_CARD_PLACEHOLDER + alt.split(" ").join("\\n")
           }
           className="w-full h-full rounded"
         />
-        <div className="hidden group-hover:block absolute inset-0 bg-black opacity-50 rounded"></div>
-        <div className="hidden group-hover:block text-white bottom-10 mx-4 absolute text-md md:text-lg font-bold z-10 rounded">
-          {alt}
+        <div className="group-hover:block absolute inset-0 bg-black opacity-50 rounded"></div>
+        <div className="group-hover:block text-white bottom-10 mx-4 absolute z-10 rounded">
+          <h3 className="text-md md:text-lg font-bold ">{alt}</h3>
+          <div className="flex items-center text-sm mt-2">
+            <VoteIcon height={13} width={13} />
+            <span className="ml-1">
+              {voteAvg} &nbsp; | &nbsp; {voteCount}
+            </span>
+          </div>
         </div>
       </div>
     </div>
