b926ce9c492a931ea8b04aa34f8bf05117e21005
Naman Jawaliya <jawaliyan@gmail.com>
2023-09-17 12:53:32 +0530

    Create useGetMovieGenre.js

---
+++ src/utils/custom-hooks/useGetMovieGenre.js
@@ -0,0 +1,27 @@
+import { useEffect } from "react";
+import { addGenre } from "../store/moviesSlice";
+import { useDispatch } from "react-redux";
+import { API_OPTIONS } from "../constants/constants";
+
+const useGetMovieGenre = () => {
+  const dispatch = useDispatch();
+  const getMovieGenres = async () => {
+    try {
+      const endpoint =
+        "https://api.themoviedb.org/3/genre/movie/list?language=en";
+      const response = await fetch(endpoint, API_OPTIONS);
+
+      const data = await response.json();
+      console.log({ data });
+      dispatch(addGenre(data.genres));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getMovieGenres();
+  }, []);
+};
+
+export default useGetMovieGenre;
