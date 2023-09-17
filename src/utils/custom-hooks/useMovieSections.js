7d43c6741e62da7217e3ff1f3eebb8053bb3f550
Naman Jawaliya <jawaliyan@gmail.com>
2023-09-17 12:54:04 +0530

    Update useMovieSections.js

--- src/utils/custom-hooks/useMovieSections.js
+++ src/utils/custom-hooks/useMovieSections.js
@@ -2,8 +2,10 @@ import usePopularMovies from "./usePopularMovies";
 import useTrendingMovies from "./useTrendingMovies";
 import useTopRatedMovies from "./useTopRatedMovies";
 import useNowPlayingMovies from "./useNowPlayingMovies";
+import useGetMovieGenre from "./useGetMovieGenre";
 
 const useMovieSections = () => {
+  useGetMovieGenre();
   useNowPlayingMovies();
   usePopularMovies();
   useTrendingMovies();
